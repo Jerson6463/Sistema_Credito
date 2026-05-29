4c67620a297a079fef901d19fafd3e2f5e42f85a
KellyCubas <Gcubaskellymerc@uss.edu.pe>
2026-05-29 00:44:28 -0500

    feat(betting): registrar ruta POST /apuestas/in-play/ para apuestas en vivo

--- betting/urls.py
+++ betting/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from betting.views import (
+    ApuestaInPlayView,
     CashOutView,
     CrearApuestaCombinada_View,
     CrearApuestaView,
@@ -13,6 +14,7 @@ urlpatterns = [
     path("eventos/", EventoListView.as_view(), name="eventos_list"),
     path("eventos/<int:pk>/", EventoDetalleView.as_view(), name="evento_detalle"),
     path("apuestas/", CrearApuestaView.as_view(), name="crear_apuesta"),
+    path("apuestas/in-play/", ApuestaInPlayView.as_view(), name="apuesta_in_play"),
     path("apuestas/mis-apuestas/", MisApuestasView.as_view(), name="mis_apuestas"),
     path("apuestas/cash-out/", CashOutView.as_view(), name="cash_out"),
     path("apuestas/combinada/", CrearApuestaCombinada_View.as_view(), name="apuesta_combinada"),
