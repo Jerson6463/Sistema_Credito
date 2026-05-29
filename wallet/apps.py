3fd632f91ad1444f7cb6073cd434fb797774f649
KellyCubas <Gcubaskellymerc@uss.edu.pe>
2026-05-29 00:42:30 -0500

    feat(wallet): agregar signal que registra cada EntradaContable en auditoria inmutable

--- wallet/apps.py
+++ wallet/apps.py
@@ -5,3 +5,6 @@ class WalletConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'wallet'
     verbose_name = 'Wallet con Partida Doble'
+
+    def ready(self):
+        import wallet.signals  # noqa: F401
